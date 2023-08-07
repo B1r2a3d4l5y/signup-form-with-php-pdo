dddf079eb58e13611ac4bbf2baa50320eff4c58c
B1r2a3d4l5y <bradleyarnold918@gmail.com>
2023-08-07 14:43:21 +0200

    edited sql code

--- sql/users.sql
+++ sql/users.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS users(
-    userid INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    userid INTEGER  NOT NULL  AUTO_INCREMENT,
     username VARCHAR(30) NOT NULL,
     email VARCHAR(255) NOT NULL,
     
-    password VARCHAR(100) NOT NULL
+    password VARCHAR(100) NOT NULL,
+    PRIMARY KEY(userid);
 );
